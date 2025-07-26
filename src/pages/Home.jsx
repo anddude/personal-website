9237bd5c6d14065480031c3e8888cb56fd8558d1
Annie Tadvick <annie.tadvick@gmail.com>
2025-07-26 12:13:09 -0400

    spacing on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,22 +25,22 @@ export default function Home() {
           <img 
           src={LongPic} 
           alt="New York City's winter" 
-          className="p-px w-auto h-85 mx-auto mb-6 object-cover rounded-sm shadow-md image-to-dim"
+          className=" w-auto h-85 mx-auto mb-6 object-cover rounded-sm shadow-md image-to-dim"
           />
           <div className="text-neonGreen font-space overlay-text">Film Photo by &nbsp;<a href="https://www.imdb.com/name/nm7670398/" target="_blank"
                 rel="noopener noreferrer" className="no-underline effect-underline">Morgan Armstrong</a></div>
           </div>
       </div>
       
-      <div>
-        <h1>
+      <div className="p-12">
+        <h1 className="pb-5">
         HOWDY <span>{cowboy}</span>
         </h1>
         <p>
           I'm Annie — a filmmaker turned software engineer with a passion for building thoughtful,
           human-centered digital experiences. After years of managing fast-paced productions and solving
           high-stakes problems behind the scenes, I pivoted into tech to bring that same creative energy and
-          attention to detail to the world wide web. Thanks for stopping by and don't be a stranger — <a href="https://calendly.com/annietadvick/30min" target="_blank"
+          attention to detail to the world wide web. Thanks for stopping by and don't be a stranger, <a href="https://calendly.com/annietadvick/30min" target="_blank"
                 rel="noopener noreferrer">
           <i><span className="hover:text-shadow-sm hover:text-shadow-sunset/50 hover:not-italic no-underline effect-underline">
           let's grab a cup of coffee and chat!</span></i></a> 
